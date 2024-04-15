2464cfae9416ed439b4a42dfcecb89a16a17520a
prodromospapa <prodromospapa@gmail.com>
2024-04-15 23:10:17 +0300

    main

--- 1.merge.py
+++ 1.merge.py
@@ -13,20 +13,33 @@ def merge(BMD_3, BMD_5,CBU_3,CBU_5,choose):
     if choose == "BMD":
         merged_data = pd.concat([data_BMD_5,only_BMD_3])
         return merged_data
-        #merged_data.to_pickle("BMD.pickle")
     elif choose == "CBU":
         merged_data = pd.concat([data_CBU_5,only_CBU_3])
         return merged_data
-        #merged_data.to_pickle("CBU.pickle")
     else:
         merged_data = pd.concat([data_BMD_5,data_CBU_5,only_CBU_3,only_BMD_3])
         return merged_data
-        #merged_data.to_pickle("all.pickle")
+    
+def merge(data):
+    loci_dict = {"A":["A1","A2"],"B":["B1","B2"],"C":["C1","C2"],"DRB1":["DRB1_1","DRB1_2"],"DQB1":["DQB1_1","DQB1_2"]}
 
-BMD_3 = "data/Greek_BMDs_2fields/76689gen_2_fields_BMDs_3loci_excl.blanks.xlsx"
-BMD_5 = "data/Greek_BMDs_2fields/70077gen_78716BMDs_2fields_5loci_excl.bl.xlsx"
-CBU_3 = "data/Greek_CBUs_2fields/3012gen_2field_CBUs_3loci_excl.bl.xlsx"
-CBU_5 = "data/Greek_CBUs_2fields/1092gen_2fields_CBUs_5loci_excl.bl.xlsx"
+    alleles_dict = {}
+    for locus in loci_dict:
+        alleles_locus = set(sorted(data[loci_dict[locus][0]].dropna().values.tolist() + data[loci_dict[locus][1]].dropna().values.tolist()))
+        alleles_dict[locus] = {}
+        n = 1
+        for allele in alleles_locus:
+            alleles_dict[locus][allele] = n
+            n += 1
+
+    final = pd.DataFrame(index = data.index)
+
+    for locus,alleles in loci_dict.items():
+        final[locus] = data[loci_dict[locus]].apply(lambda x: set(sorted([alleles_dict[locus][i] for i in x if i in alleles_dict[locus]])), axis=1)
+        #final[alleles[0]] = data[alleles[0]].apply(lambda x: alleles_dict[locus][x] if x in alleles_dict[locus] else 0)
+        #final[alleles[1]] = data[alleles[1]].apply(lambda x: alleles_dict[locus][x] if x in alleles_dict[locus] else 0)
+
+    final.to_pickle(f"{args.choose}.pickle")
 
 parser = argparse.ArgumentParser(
                     prog='ProgramName',
@@ -34,27 +47,14 @@ parser = argparse.ArgumentParser(
                     epilog='Text at the bottom of help')
 
 parser.add_argument('--choose','-c', choices=['BMD','CBU','all'], help='Direction(GvH or HvG)', required=True)
-
 args = parser.parse_args()
 
+BMD_3 = "data/Greek_BMDs_2fields/76689gen_2_fields_BMDs_3loci_excl.blanks.xlsx"
+BMD_5 = "data/Greek_BMDs_2fields/70077gen_78716BMDs_2fields_5loci_excl.bl.xlsx"
+CBU_3 = "data/Greek_CBUs_2fields/3012gen_2field_CBUs_3loci_excl.bl.xlsx"
+CBU_5 = "data/Greek_CBUs_2fields/1092gen_2fields_CBUs_5loci_excl.bl.xlsx"
+
+
 data = merge(BMD_3, BMD_5,CBU_3,CBU_5, args.choose)
+merge(data)
 
-loci_dict = {"A":["A1","A2"],"B":["B1","B2"],"C":["C1","C2"],"DRB1":["DRB1_1","DRB1_2"],"DQB1":["DQB1_1","DQB1_2"]}
-
-alleles_dict = {}
-for locus in loci_dict:
-    alleles_locus = set(sorted(data[loci_dict[locus][0]].dropna().values.tolist() + data[loci_dict[locus][1]].dropna().values.tolist()))
-    alleles_dict[locus] = {}
-    n = 1
-    for allele in alleles_locus:
-        alleles_dict[locus][allele] = n
-        n += 1
-
-final = pd.DataFrame(index = data.index)
-
-for locus,alleles in loci_dict.items():
-    final[locus] = data[loci_dict[locus]].apply(lambda x: set(sorted([alleles_dict[locus][i] for i in x if i in alleles_dict[locus]])), axis=1)
-    #final[alleles[0]] = data[alleles[0]].apply(lambda x: alleles_dict[locus][x] if x in alleles_dict[locus] else 0)
-    #final[alleles[1]] = data[alleles[1]].apply(lambda x: alleles_dict[locus][x] if x in alleles_dict[locus] else 0)
-
-final.to_pickle(f"{args.choose}.pickle")
