ca3ae7f6f6f9b46287e0ef8f47fe5f353a68e7f2
prodromospapa <prodromospapa@gmail.com>
2024-04-15 23:08:47 +0300

    main

---
+++ mm_per.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+f = open(f'ouput_HvG.npy', 'rb')
+data = np.load(f)
+arr = np.ones((len(part), len(data_np), len(loci_index_list)), dtype=np.int8)
+loci_index = {"A":0,"B":1,"C":2,"DRB1":3,"DQB1":4}
+for sample in data:
+    print(sample[i,:,loci])
