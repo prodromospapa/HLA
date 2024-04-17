bae8b94f78f7d94382ac11b967064ce4ef5544e0
prodromospapa <prodromospapa@gmail.com>
2024-04-17 12:55:41 +0300

    main

--- mm_bar.py
+++ mm_bar.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import argparse
+from statistics import mean
 
 def bar(choose,loci,direction):
     with open(f'{choose}_{direction}.npy', 'rb') as f:
@@ -12,6 +13,9 @@ def bar(choose,loci,direction):
         all = all[all['source']== choose].reset_index(drop=True)
     id = all["ID"].tolist()
     loci_dict = {"A":0,"B":1,"C":2,"DRB1":3,"DQB1":4,'3':1,'5': 2}
+    y1_total = []
+    y2_total = []
+    y3_total = []
     for i,x in enumerate(id):
         if choose in ['3','5']:
             row = data[i][loci_dict[loci]]
@@ -19,12 +23,20 @@ def bar(choose,loci,direction):
             row = data[i][0][loci_dict[loci]]
         if row != None:
             y1 = row[0]
+            y1_total.append(y1)
             plt.bar(x,y1,color='b')
+            
             y2 = row[1]
+            y2_total.append(y2)
             plt.bar(x,y2,bottom=y1,color='r')
+
             y3 = row[2]
-            plt.bar(x,y3,bottom=y2,color='g')
+            y3_total.append(y3)
+            plt.bar(x,y3,bottom=y1+y2,color='g')    
         print(f"{i+1}/{len(id)}",end="\r")
+    plt.axhline(mean(y1_total), color='blue')
+    plt.axhline(mean(y2_total), color='red')
+    plt.axhline(mean(y3_total), color='green')
     plt.title(f"{choose}_{direction}_{loci}")
     plt.savefig(f"{choose}_{direction}_{loci}.png")
 
