97c09ad56995f03fee8dbb2171c4a50858b7b7a9
prodromospapa <prodromospapa@gmail.com>
2024-04-19 20:19:43 +0300

    main

--- helen_ranking_alleles.py
+++ helen_ranking_alleles.py
@@ -10,6 +10,8 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('--choose','-c', choices=['BMD','CBU','all'],type=str, help='Direction(GvH or HvG)', required=True)
 parser.add_argument('--direction','-d', choices=['GvH','HvG'], help='Direction(GvH or HvG)', required=True)
+parser.add_argument('--plot','-p', choices=['True','False'], help='', required=True)
+parser.add_argument('--ranking','-r', help='Direction(GvH or HvG)', required=True,type=str)
 
 args = parser.parse_args()
 
@@ -19,36 +21,37 @@ al_per_df = pd.read_pickle(f"{args.choose}_{args.direction}_al_per.pickle")
 mm_per = mm_per_df
 al_per = al_per_df.reindex(mm_per.index)
 
-plot = True
-for ranking in [[1,0.1],[0.1,0.05],[0.05,0]]:
-        #ranking_values_list = [y for x in mm_per[al_per<ranking[0]][al_per >=ranking[1]].values.tolist() for y in x if type(y).__name__=="dict"]
-        ranking_values_list = [[x[0]+"_"+base.name,x[1]] for index,base in mm_per[al_per<ranking[0]][al_per >=ranking[1]].iterrows() if base.dropna().values.all() for x in base.dropna().items()]
-        zeros = sorted([[sample[0],sample[1][0]] for sample in ranking_values_list],key=lambda x: x[1])
-        ones = sorted([[sample[0],sample[1][1]] for sample in ranking_values_list],key=lambda x: x[1])
-        twos = sorted([[sample[0],sample[1][2]] for sample in ranking_values_list],key=lambda x: x[1])
-        if plot:
-                plt.plot([x[0] for x in zeros],[x[1] for x in zeros],color="blue")
-                plt.title(f"{args.choose}_{args.direction}_ranking_{ranking[0]}_{ranking[1]}_zeros")
-                plt.xticks(rotation=90)
-                plt.tight_layout()
-                plt.savefig(f"{args.choose}_{args.direction}_ranking_{ranking[0]}_{ranking[1]}_zeros.png",dpi=300)
-                plt.close()
-                
-                plt.plot([x[0] for x in ones],[x[1] for x in ones],color="red")
-                plt.title(f"{args.choose}_{args.direction}_ranking_{ranking[0]}_{ranking[1]}_ones")
-                plt.xticks(rotation=90)
-                plt.tight_layout()
-                plt.savefig(f"{args.choose}_{args.direction}_ranking_{ranking[0]}_{ranking[1]}_ones.png",dpi=300)
-                plt.close()
 
-                plt.plot([x[0] for x in twos],[x[1] for x in twos],color="green")
-                plt.title(f"{args.choose}_{args.direction}_ranking_{ranking[0]}_{ranking[1]}_twos")
-                plt.xticks(rotation=90)
-                plt.tight_layout()
-                plt.savefig(f"{args.choose}_{args.direction}_ranking_{ranking[0]}_{ranking[1]}_twos.png",dpi=300)
-                plt.close()
-        else:
-                print(f"{ranking}: zeros: {mean([x[1] for x in zeros])}, ones: {mean([x[1] for x in ones])}, twos: {mean([x[1] for x in twos])}")
+def ranking_plot(plot,ranking):
+        for ranking in ranking:
+                ranking_values_list = [[x[0]+"_"+base.name,x[1]] for index,base in mm_per[al_per<ranking[0]][al_per >=ranking[1]].iterrows() if base.dropna().values.all() for x in base.dropna().items()]
+                zeros = sorted([[sample[0],sample[1][0]] for sample in ranking_values_list],key=lambda x: x[1])
+                ones = sorted([[sample[0],sample[1][1]] for sample in ranking_values_list],key=lambda x: x[1])
+                twos = sorted([[sample[0],sample[1][2]] for sample in ranking_values_list],key=lambda x: x[1])
+                if plot:
+                        plt.plot([x[0] for x in zeros],[x[1] for x in zeros],color="blue")
+                        plt.title(f"{args.choose}_{args.direction}_ranking_{ranking[0]}_{ranking[1]}_zeros")
+                        plt.xticks(rotation=90)
+                        plt.tight_layout()
+                        plt.savefig(f"{args.choose}_{args.direction}_ranking_{ranking[0]}_{ranking[1]}_zeros.png",dpi=300)
+                        plt.close()
+                        
+                        plt.plot([x[0] for x in ones],[x[1] for x in ones],color="red")
+                        plt.title(f"{args.choose}_{args.direction}_ranking_{ranking[0]}_{ranking[1]}_ones")
+                        plt.xticks(rotation=90)
+                        plt.tight_layout()
+                        plt.savefig(f"{args.choose}_{args.direction}_ranking_{ranking[0]}_{ranking[1]}_ones.png",dpi=300)
+                        plt.close()
 
+                        plt.plot([x[0] for x in twos],[x[1] for x in twos],color="green")
+                        plt.title(f"{args.choose}_{args.direction}_ranking_{ranking[0]}_{ranking[1]}_twos")
+                        plt.xticks(rotation=90)
+                        plt.tight_layout()
+                        plt.savefig(f"{args.choose}_{args.direction}_ranking_{ranking[0]}_{ranking[1]}_twos.png",dpi=300)
+                        plt.close()
+                else:
+                        print(f"{ranking}: zeros: {mean([x[1] for x in zeros])}, ones: {mean([x[1] for x in ones])}, twos: {mean([x[1] for x in twos])}")
 
-#python3 helen_ranking.py -c CBU -d HvG
+ranking_plot(args.plot,eval(args.ranking))
+
+#python3 helen_ranking_alleles.py -c CBU -d HvG -p True -r [[1,0.1],[0.1,0.05]]
