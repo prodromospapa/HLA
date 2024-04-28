675b3c02d80818533c9e37ceab81f5e5d9e8e02b
prodromospapa <prodromospapa@gmail.com>
2024-04-28 20:59:44 +0300

    main

--- 1.3.3.mm_ranking.py
+++ 1.3.3.mm_ranking.py
@@ -6,11 +6,8 @@ import matplotlib.pyplot as plt
 
 def fontsizer(df):
         num_labels = len(df)
-        if num_labels > 0:
-                plot_width = plt.gcf().get_size_inches()[0]  # Get the width of the plot in inches
-                return plot_width / num_labels * 72  # The '72' is to convert from inches to points
-        else:
-                return 8
+        plot_width = plt.gcf().get_size_inches()[0]  # Get the width of the plot in inches
+        return plot_width / num_labels * 72  # The '72' is to convert from inches to points
 
 
 parser = argparse.ArgumentParser(
@@ -38,60 +35,62 @@ if not os.path.exists('plots'):
 dpi = args.dpi
 def ranking_plot(plot,ranking_list,sort):
         for ranking in ranking_list:
-                #alleles
-                ranking_values_list = [[x[0]+"_"+base.name,x[1]] for index,base in mm_per[freq_per<ranking[0]][freq_per >=ranking[1]].iterrows() if base.dropna().values.all() for x in base.dropna().items()]
-                sort_way = 0 if sort == "name" else 1
-                zeros = sorted([[sample[0],sample[1][0]] for sample in ranking_values_list],key=lambda x: x[sort_way])
-                ones = sorted([[sample[0],sample[1][1]] for sample in ranking_values_list],key=lambda x: x[sort_way])
-                twos = sorted([[sample[0],sample[1][2]] for sample in ranking_values_list],key=lambda x: x[sort_way])
-                #total
-                ranking_values_list_total = [y for x in mm_per[freq_per<ranking[0]][freq_per >=ranking[1]].values.tolist() for y in x if type(y).__name__=="dict"]
-                zeros_total = [dictionary[0] for dictionary in ranking_values_list_total]
-                ones_total = [dictionary[1] for dictionary in ranking_values_list_total]
-                twos_total = [dictionary[2] for dictionary in ranking_values_list_total]
-                if plot:
-                        if sort_way == 0:
-                                plt.plot([x[0] for x in zeros],[x[1] for x in zeros],color="blue")
-                                plt.plot([x[0] for x in ones],[x[1] for x in ones],color="red")
-                                plt.plot([x[0] for x in twos],[x[1] for x in twos],color="green")
-                                plt.title(f"{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}_sortbyname")
-                                plt.xticks(rotation=90,fontsize=fontsizer(zeros) if fontsizer(zeros) < 8 else 8)
+                values = mm_per[freq_per<ranking[0]][freq_per >=ranking[1]].dropna(how="all")
+                if not values.empty:
+                        #alleles
+                        ranking_values_list = [[x[0]+"_"+base.name,x[1]] for index,base in values.iterrows() if base.dropna().values.all() for x in base.dropna().items()]
+                        sort_way = 0 if sort == "name" else 1
+                        zeros = sorted([[sample[0],sample[1][0]] for sample in ranking_values_list],key=lambda x: x[sort_way])
+                        ones = sorted([[sample[0],sample[1][1]] for sample in ranking_values_list],key=lambda x: x[sort_way])
+                        twos = sorted([[sample[0],sample[1][2]] for sample in ranking_values_list],key=lambda x: x[sort_way])
+                        #total
+                        ranking_values_list_total = [y for x in values.iterrows() for y in x if type(y).__name__=="dict"]
+                        zeros_total = [dictionary[0] for dictionary in ranking_values_list_total]
+                        ones_total = [dictionary[1] for dictionary in ranking_values_list_total]
+                        twos_total = [dictionary[2] for dictionary in ranking_values_list_total]
+                        if plot:
+                                if sort_way == 0:
+                                        plt.plot([x[0] for x in zeros],[x[1] for x in zeros],color="blue")
+                                        plt.plot([x[0] for x in ones],[x[1] for x in ones],color="red")
+                                        plt.plot([x[0] for x in twos],[x[1] for x in twos],color="green")
+                                        plt.title(f"{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}_sortbyname")
+                                        plt.xticks(rotation=90,fontsize=fontsizer(zeros) if fontsizer(zeros) < 8 else 8)
+                                        plt.legend(["0","1","2"])
+                                        plt.tight_layout()
+                                        plt.savefig(f"plots/{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}_sortbyname.png",dpi=dpi)
+                                        plt.close()
+                                else:
+                                        plt.plot([x[0] for x in zeros],[x[1] for x in zeros],color="blue")
+                                        plt.title(f"{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}_zeros_sortbyvalue")
+                                        plt.xticks(rotation=90,fontsize=fontsizer(zeros) if fontsizer(zeros) < 8 else 8)
+                                        plt.tight_layout()
+                                        plt.savefig(f"plots/{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}_zeros_sortbyvalue.png",dpi=dpi)
+                                        plt.close()
+                                        
+                                        plt.plot([x[0] for x in ones],[x[1] for x in ones],color="red")
+                                        plt.title(f"{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}_ones_sortbyvalue")
+                                        plt.xticks(rotation=90,fontsize=fontsizer(ones) if fontsizer(ones) < 8 else 8)
+                                        plt.tight_layout()
+                                        plt.savefig(f"plots/{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}_ones_sortbyvalue.png",dpi=dpi)
+                                        plt.close()
+
+                                        plt.plot([x[0] for x in twos],[x[1] for x in twos],color="green")
+                                        plt.title(f"{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}_twos_sortbyvalue")
+                                        plt.xticks(rotation=90,fontsize=fontsizer(twos) if fontsizer(twos) < 8 else 8)
+                                        plt.tight_layout()
+                                        plt.savefig(f"plots/{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}_twos_sortbyvalue.png",dpi=dpi)
+                                        plt.close()
+                                #plot total
+                                plt.plot(sorted(zeros_total),color="blue")
+                                plt.plot(sorted(ones_total),color="red")
+                                plt.plot(sorted(twos_total),color="green")
                                 plt.legend(["0","1","2"])
+                                plt.title(f"{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}")
                                 plt.tight_layout()
-                                plt.savefig(f"plots/{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}_sortbyname.png",dpi=dpi)
+                                plt.savefig(f"plots/{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}_total.png",dpi=dpi)
                                 plt.close()
                         else:
-                                plt.plot([x[0] for x in zeros],[x[1] for x in zeros],color="blue")
-                                plt.title(f"{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}_zeros_sortbyvalue")
-                                plt.xticks(rotation=90,fontsize=fontsizer(zeros) if fontsizer(zeros) < 8 else 8)
-                                plt.tight_layout()
-                                plt.savefig(f"plots/{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}_zeros_sortbyvalue.png",dpi=dpi)
-                                plt.close()
-                                
-                                plt.plot([x[0] for x in ones],[x[1] for x in ones],color="red")
-                                plt.title(f"{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}_ones_sortbyvalue")
-                                plt.xticks(rotation=90,fontsize=fontsizer(ones) if fontsizer(ones) < 8 else 8)
-                                plt.tight_layout()
-                                plt.savefig(f"plots/{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}_ones_sortbyvalue.png",dpi=dpi)
-                                plt.close()
-
-                                plt.plot([x[0] for x in twos],[x[1] for x in twos],color="green")
-                                plt.title(f"{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}_twos_sortbyvalue")
-                                plt.xticks(rotation=90,fontsize=fontsizer(twos) if fontsizer(twos) < 8 else 8)
-                                plt.tight_layout()
-                                plt.savefig(f"plots/{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}_twos_sortbyvalue.png",dpi=dpi)
-                                plt.close()
-                        #plot total
-                        plt.plot(sorted(zeros_total),color="blue")
-                        plt.plot(sorted(ones_total),color="red")
-                        plt.plot(sorted(twos_total),color="green")
-                        plt.legend(["0","1","2"])
-                        plt.title(f"{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}")
-                        plt.tight_layout()
-                        plt.savefig(f"plots/{args.choose}_{args.direction}_{args.type}_ranking_{ranking[0]}_{ranking[1]}_total.png",dpi=dpi)
-                        plt.close()
-                else:
-                        print(f"{ranking}: zeros: {mean([x[1] for x in zeros])}, ones: {mean([x[1] for x in ones])}, twos: {mean([x[1] for x in twos])}")
+                                print(f"{ranking}: zeros: {mean([x[1] for x in zeros])}, ones: {mean([x[1] for x in ones])}, twos: {mean([x[1] for x in twos])}")
 
 ranking_plot(args.plot,eval(args.ranking),args.sort)
 
